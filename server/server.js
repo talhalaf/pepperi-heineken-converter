174a54d83bd534748d2089c2d49240503712515b
Tal Halaf <talhalaf@outlook.com>
2018-06-25 00:29:39 +0300

    Added send PDF as attachment

--- server/server.js
+++ server/server.js
@@ -28,6 +28,50 @@ app.get('/PDF/:ActivityID',authenticate,(request,response) => {
         });},1000);
     });
 
+app.get('/PDFAttachment/:ActivityID',authenticate,(request,response) => {
+    let ActivityID = request.params.ActivityID;
+    setTimeout(()=>{ 
+        getPDFObject(ActivityID,request.token)
+        .then((res)=>{
+            console.log('All data for PDF was generated succesfully!');
+            sendPDFAttachment(response,res.newPDFFilePath,1000);
+        })
+        .catch(e=>{
+            console.log("error creating PDF:",e,"Sending 404.html");
+            response.sendFile(publicPath+'/404.html');
+        });},1000);
+    });
+let sendPDFAttachment = (response,newPDFFilePath,timeToWait,numberOfTries) => {
+    numberOfTries = numberOfTries || 1;
+    console.log('Waiting '+timeToWait/1000+' second(s)...');
+    if (timeToWait/1000 > 10){
+        throw new Error('File Not Found!');
+    }
+    else {
+        setTimeout(()=>{
+            fs.stat(newPDFFilePath,(err,stats)=>{
+
+                //File is not ready yet, redirecting to the same URL.
+                if (!stats){
+                    console.log('--------------------------------');
+                    console.log('Could not locate the file on the Machine yet...')
+                    console.log('--------------------------------');
+                    timeToWait = numberOfTries%2 == 0 ? timeToWait*1.5 : timeToWait;
+                    sendPDFFile(response,newPDFFilePath,timeToWait,numberOfTries+1);
+                }
+                else if (!err){
+                    console.log('--------------------------------');
+                    console.log('Success! PDF was created on the machine...')
+                    console.log('--------------------------------');
+                    fileLocated = true;
+                    response.attachment(newPDFFilePath).sendFile(newPDFFilePath);
+                } else {
+                    console.log(error);
+                }
+            });
+        },timeToWait);
+    }
+}
 
 let sendPDFFile = (response,newPDFFilePath,timeToWait,numberOfTries) => {
     numberOfTries = numberOfTries || 1;
